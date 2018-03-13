b10a96e962a185bcfd64106bf42e25aaf470d16e
JoaoCastro84 <37122903+JoaoCastro84@users.noreply.github.com>
2018-03-13 17:05:05 -0300

    Add files via upload

---
+++ aula 3 python.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 13 16:45:53 2018
+
+@author: joaoc
+"""
+
+lista2=[]
+numero = [0,0,0,0,0]
+for i in range(0,5):
+    numero[i] = int(input("\nDigite um numero"))
+    print (numero)
+
---
+++ lista com while aula 3.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 13 16:51:35 2018
+
+@author: joaoc
+"""
+
+lista=[]
+x=1
+
+while x>0:
+    x=int(input("Digite um número"))
+    if x!=0:
+        lista.append(x)
+        
+        
+print(lista)        
